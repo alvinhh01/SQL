954f7c0a960032b9b0f31846d2cd4e4a0c548c27
alvinhh01 <133548236+alvinhh01@users.noreply.github.com>
2023-12-13 14:05:40 +0800

    DATAChapter5_Q1-2.sql

--- DATA5_1-2.sql
+++ DATA5_1-2.sql
@@ -1,3 +1,12 @@
+--Dataset: invoices
+--Source: SQL QuickStart Guide: The Simplified Beginner's Guide to Managing, Analyzing, and Manipulating Data With SQL (https://www.amazon.sg/SQL-QuickStart-Guide-Simplified-Manipulating/dp/1945051752)
+--Queried using: DB Browser using SQLite
+
+**Chapter 5 exercises**
+-- Q1. Create a query for the invoices table that includes a CASE statement that labels all sales from billing country USA as “Domestic Sales” and all other sales as “Foreign Sales.” 
+        Label your new field as SalesType after your END AS statement.
+-- Q2. Order this data by the new field SalesType.
+    
 SELECT 
     InvoiceDate,
     BillingAddress,
