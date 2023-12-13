70c4c59e3a0a3ca7e73e32f1d287e63f2dc7f8a5
alvinhh01 <133548236+alvinhh01@users.noreply.github.com>
2023-12-13 14:06:23 +0800

    DATAChapter5_Q3.sql

--- DATA5_3.sql
+++ DATA5_3.sql
@@ -1,3 +1,10 @@
+--Dataset: invoices
+--Source: SQL QuickStart Guide: The Simplified Beginner's Guide to Managing, Analyzing, and Manipulating Data With SQL (https://www.amazon.sg/SQL-QuickStart-Guide-Simplified-Manipulating/dp/1945051752)
+--Queried using: DB Browser using SQLite
+
+**Chapter 5 exercises**
+-- 3. How many invoices from Domestic Sales were over $15?
+    
 SELECT 
     InvoiceDate,
     BillingAddress,
